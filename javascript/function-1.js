8d32a41516e79b906271d0275c0951171047eba0
DBiz <dbiz@DBITLAP0585.local>
2024-07-09 11:36:14 +0530

    java script tasks

---
+++ javascript/function-1.js
@@ -0,0 +1,55 @@
+// console.log(val);
+// func();
+func1();
+
+let val = 1;
+
+let func = function () {
+  console.log("test");
+};
+
+let func1 = () => {
+  console.log("test 1");
+};
+
+// const video = {
+//   title: "a",
+//   tags: [1, 2, 3, 4],
+//   showTags() {
+//     this.tags.forEach(function (e) {
+//       console.log(this.title, e);
+//     });
+//   },
+// };
+
+// video.showTags();
+
+// function createCircle(radius) {
+//   return {
+//     radius,
+//     draw() {
+//       console.log("creating circle" + radius);
+//     },
+//   };
+// }
+
+// const val = {
+//   v: 1,
+//   draw() {
+//     console.log("test");
+//   },
+// };
+
+// for (let key in val) {
+//   console.log(key, val[key]);
+// }
+
+// for (let key of Object.keys(val)) {
+//   console.log(key);
+// }
+
+// for (let key of Object.entries(val)) {
+//   console.log(key);
+// }
+
+// createCircle(1).draw();
